135582bb7faaf57899e4b177e9eac712e0a017f1
Roberto <roby.divia@gmail.com>
2023-03-09 18:06:43 +0100

    GamePause UI

---
+++ Assets/Scripts/GamePauseUI.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GamePauseUI : MonoBehaviour
+{
+
+    public static bool isGamePaused = false;
+    public GameObject pausedMenuUI; // Update is called once per frame
+
+    [SerializeField] private Button resumeButton;
+    [SerializeField] private Button menuButton;
+    [SerializeField] private Button quitButton;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            TogglePauseGame();
+        }
+    }
+    /*
+    public void Resume(){
+        pausedMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        
+    }
+    
+    void Pause(){
+        pausedMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        GameIsPaused = true;
+    }
+    */
+
+    public void TogglePauseGame() {
+        isGamePaused = !isGamePaused;
+        if (isGamePaused) {
+            Time.timeScale = 0f;    
+            pausedMenuUI.SetActive(true);  
+        } else {
+            Time.timeScale = 1f;
+            pausedMenuUI.SetActive(false);
+        }  
+    }
+
+
+
+    private void Awake(){
+        resumeButton.onClick.AddListener(() =>
+        {
+            TogglePauseGame();
+        });
+
+        menuButton.onClick.AddListener(() =>
+        {
+            TogglePauseGame();
+            SceneManager.LoadScene("MainMenuScene");
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+            TogglePauseGame();
+            Application.Quit();
+        });
+
+    }
+
+}
+
